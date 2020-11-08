8fb525a6b4d57c968f524493afa1e62cb0610172
Yasmin Sahraoui <ysahraou@ford.com>
2020-11-08 22:09:05 +0000

    working

--- src/test/java/MarsRoverTest.kt
+++ src/test/java/MarsRoverTest.kt
@@ -5,11 +5,14 @@ class MarsRoverTest {
 
     @Test
     fun roverMovesOneForward() {
-        val rover = MarsRover()
-        val expectedX = 5
+        val direction = "N"
+        val startX = 5
+        val startY = 5
+        val rover = MarsRover(direction, startX, startY)
 
         rover.forward()
 
-        assertEquals(expectedX, rover.x())
+        assertEquals(5, rover.x)
+        assertEquals(6, rover.y)
     }
 }
